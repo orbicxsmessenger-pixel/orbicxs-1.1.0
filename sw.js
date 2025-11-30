b2346c52d855e8548de26e086ed9be1903f6601e
orbicxsmessenger-pixel <orbicxsmessenger@gmail.com>
2025-11-30 15:31:01 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,4 @@
-const CACHE = 'orbicxs-v3';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png']))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
-
-self.addEventListener('push', e => {
-  const data = e.data?.json() || {title:'OrbicXs', body:'Yangi xabar!'};
-  e.waitUntil(self.registration.showNotification(data.title, {body:data.body, icon:'/icon-192.png', badge:'/icon-192.png', vibrate:[200,100,200]}));
-});
-
-self.addEventListener('notificationclick', e => {
-  e.notification.close();
-  e.waitUntil(clients.openWindow('/'));
-});
+const C='v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png']))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.openWindow('/'));});
